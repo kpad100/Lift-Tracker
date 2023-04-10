e9fc64b42d2e0c9ec156c501c93c87cf66d95278
Milan Trivedi <trivedimilan8@gmail.com>
2023-04-10 12:44:40 -0400

    made changes to analyzer

--- app/src/main/java/com/example/lifttracker/analyze.java
+++ app/src/main/java/com/example/lifttracker/analyze.java
@@ -1,36 +1,60 @@
+package com.example.lifttracker;
+
 import java.io.IOException;
+
+import java.util.Arrays;
 
 public class analyze {
 
-    private double[] dataX;
-    private double[] dataY;
+
 
     private double[] positionX;
-    private double[] positionY;
+    private double[] time;
+    private double[] velocity;
+    private double[] acceleration;
 
-    private double[] velocityX;
-    private double[] velocityY;
+    public analyze(double[] positionX, double[] positionY) {
+        this.positionX = positionX;
+        time = positionY;
 
-    private double[] accelerationX;
-    private double[] accelerationY;
+        makevelocity();
+        makeaccel();
+    }
 
-    public analyze(double[] dataX, double[] dataY) {
-        this.dataX = dataX;
-        this.dataY = dataY;
+    private void makevelocity(){
+        velocity= new double[positionX.length];
+
+        // Calculate the velocity for each time step
+        for (int i = 1; i < positionX.length; i++) {
+            velocity[i] = (positionX[i] - positionX[i-1]) / (time[i] - time[i-1]);
+        }
     }
 
 
+    public double[] getvelocity() {
 
+        return velocity;
+    }
 
+    private void makeaccel(){
+        acceleration= new double[velocity.length];
 
+        // Calculate the acceleration for each time step
+        for (int i = 1; i < velocity.length; i++) {
+            acceleration[i] = (velocity[i] - velocity[i-1]) / (time[i] - time[i-1]);
+        }
+    }
 
+    public double[] getaccel() {
+        return acceleration;
+    }
 
     public static void main(String args[]) throws IOException {
-        analyze analyzer = new analyze()
-
-
-
-
+        double[] xValues = {1.2, 2.4, 3.1, 4.5, 5.7, 6.3, 7.1, 8.2, 9.0, 10.2};
+        double[] yValues = {4.5, 5.7, 6.3, 7.1, 8.2, 9.0, 10.2, 11.3, 12.1, 13.4};
+        analyze analyzer = new analyze(xValues, yValues);
+        System.out.println(Arrays.toString(analyzer.getvelocity()));
+        System.out.println(Arrays.toString(analyzer.getaccel()));
 
     }
 
