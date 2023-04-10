23465d6a367f9d1e242dbb71a635beb6f69492d7
Milan Trivedi <trivedimilan8@gmail.com>
2023-04-10 12:33:35 -0400

    added changes to analyzer

---
+++ app/src/main/java/com/example/lifttracker/analyze.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+
+public class analyze {
+
+    private double[] dataX;
+    private double[] dataY;
+
+    private double[] positionX;
+    private double[] positionY;
+
+    private double[] velocityX;
+    private double[] velocityY;
+
+    private double[] accelerationX;
+    private double[] accelerationY;
+
+    public analyze(double[] dataX, double[] dataY) {
+        this.dataX = dataX;
+        this.dataY = dataY;
+    }
+
+
+
+
+
+
+
+    public static void main(String args[]) throws IOException {
+        analyze analyzer = new analyze()
+
+
+
+
+
+    }
+
+
+}
